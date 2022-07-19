5ec492553465f3b18354255cff33e0e6b4853779
IholdthedoorI <104304593+IholdthedoorI@users.noreply.github.com>
2022-07-20 02:01:11 +0300

    Add files via upload

---
+++ 2-DATA TYPES (DAY 1).cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int i = 4;
+        double d = 4.0;
+        string s = "HackerRank ";
+
+
+
+        int x;
+        double y;
+        string z;
+
+
+        x = Convert.ToInt32(Console.ReadLine());
+        y = Convert.ToDouble(Console.ReadLine());
+        z = Console.ReadLine();
+        double toplam = d + y;
+
+
+
+
+        string y1 = toplam.ToString("0.0");
+
+        Console.WriteLine(i + x);
+        Console.WriteLine(y1);
+        Console.WriteLine(s + z);
+    }
+}
